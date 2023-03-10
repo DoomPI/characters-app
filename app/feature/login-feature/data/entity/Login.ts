272803f22c034185a09e2a126a3e0f0cd899ab5c
DoomPI <rslomtev@edu.hse.ru>
2023-03-10 20:45:00 +0300

    Удалил неиспользуемые файлы

--- app/feature/login-feature/data/entity/Login.ts
+++
@@ -1,9 +0,0 @@
-export class Login {
-    name: string
-    password: string
-
-    constructor(name: string, password: string) {
-        this.name = name
-        this.password = password
-    }
-}
